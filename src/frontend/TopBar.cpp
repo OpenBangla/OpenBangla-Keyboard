72be09926b1bb671bd11fbefc267c72a463d74dc
mominul <nahidbinbaten1995@gmail.com>
2016-02-07 14:36:30 +0600

    Experimental TopBar moving support.

--- src/frontend/TopBar.cpp
+++ src/frontend/TopBar.cpp
@@ -1,3 +1,5 @@
+#include <QMessageBox>
+#include <QMouseEvent>
 #include "TopBar.h"
 #include "ui_TopBar.h"
 
@@ -6,9 +8,35 @@ TopBar::TopBar(QWidget *parent) :
     ui(new Ui::TopBar)
 {
     ui->setupUi(this);
+
+    //connect(ui->buttonIcon, SIGNAL(mouseMoveEvent(QMouseEvent*)), this, SLOT(mouseMoveButtonIcon(QMouseEvent*)));
+    this->setWindowFlags(Qt::Window | Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
 }
 
 TopBar::~TopBar()
 {
     delete ui;
+}
+
+void TopBar::on_buttonAbout_clicked()
+{
+    QMessageBox::about(this, "About OpenBangla Keyboard",
+        tr("<h2>OpenBangla Keyboard 0.1</h2>"
+        "<p>Copyright &copy; 2015-2016 Muhammad Mominul Huque & OpenBangla Team."
+        "<p>An OpenSource, Cross-Platform, Unicode Compliant Bengali Input Method."));
+}
+
+void TopBar::mouseMoveEvent(QMouseEvent *event) {
+  move(event->globalX() - pressedMouseX, event->globalY() - pressedMouseY);
+}
+
+void TopBar::mousePressEvent(QMouseEvent *event) {
+  pressedMouseX = event->x();
+  pressedMouseY = event->y();
+  event->accept();
+}
+
+void TopBar::on_buttonIcon_clicked()
+{
+    TopBar::close();
 }
--- src/frontend/TopBar.h
+++ src/frontend/TopBar.h
@@ -15,8 +15,17 @@ public:
     explicit TopBar(QWidget *parent = 0);
     ~TopBar();
 
+protected:
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+
+private slots:
+    void on_buttonAbout_clicked();
+    void on_buttonIcon_clicked();
+
 private:
     Ui::TopBar *ui;
+    int pressedMouseX, pressedMouseY;
 };
 
 #endif // TOPBAR_H
