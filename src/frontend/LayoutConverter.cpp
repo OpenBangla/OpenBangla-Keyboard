33bae515c82d7873c4444164d7e78a5f3c2b33c0
Muhammad Mominul Huque <mominul2082@gmail.com>
2018-04-10 17:30:04 +0600

    Rewrite LayoutConverter using QXmlStreamReader :tada: Remove the dependency on Qt5Xml library

--- src/frontend/LayoutConverter.cpp
+++ src/frontend/LayoutConverter.cpp
@@ -1,6 +1,6 @@
 /*
  *  OpenBangla Keyboard
- *  Copyright (C) 2016 Muhammad Mominul Huque <mominul2082@gmail.com>
+ *  Copyright (C) 2016-2018 Muhammad Mominul Huque <mominul2082@gmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,14 +16,20 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <QFile>
 #include <QFileInfo>
+#include <QXmlStreamReader>
+#include <QJsonDocument>
+#include <QJsonObject>
 #include "LayoutConverter.h"
 
-void LayoutConverter::convertLayout(QString path) {
+void LayoutConverter::convertLayout(QString path)
+{
   convertAvroLayout(path);
 }
 
-QString LayoutConverter::UnescapeXML(QString escaped) {
+QString LayoutConverter::unescapeXML(QString escaped)
+{
   escaped.replace("&amp;", "&");
   escaped.replace("&lt;", "<");
   escaped.replace("&gt;", ">");
@@ -32,65 +38,115 @@ QString LayoutConverter::UnescapeXML(QString escaped) {
   return escaped;
 }
 
-void LayoutConverter::convertAvroLayout(QString path) {
-  // Open the to-be converted
-  conversionFile.setFileName(path);
-  conversionFile.open(QIODevice::ReadOnly);
-  data = conversionFile.readAll();
+void LayoutConverter::convertAvroLayout(QString path)
+{
+  QFile xmlFile(path);
+  xmlFile.open(QIODevice::ReadOnly | QIODevice::Text);
+  QByteArray xmlData = xmlFile.readAll();
+  QXmlStreamReader *xmlReader = new QXmlStreamReader(xmlData);
+  QJsonObject layoutDev, infoLayout, info, keys;
 
-  QDomDocument avrolayout;
-  avrolayout.setContent(data);
-  layoutRoot = avrolayout.documentElement();
-
-  convertAvroInfo();
-  convertAvroKeydata();
-
-  /**/
-  QFileInfo info(conversionFile);
-  QString savePath = folders.getUserLayoutPath() + info.baseName() + ".json";
-
-  std::ofstream ofs;
-  ofs.open(savePath.toStdString(), std::ofstream::out);
-  ofs << std::setw(4) << jsonFile;
-
-  conversionFile.close();
-  ofs.close();
-}
-
-void LayoutConverter::convertAvroInfo() {
-  jsonFile["info"]["type"] = "fixed";
-  jsonFile["info"]["version"] = "1";
-  jsonFile["info"]["layout"]["name"] = layoutRoot.firstChild().nextSiblingElement("LayoutName").text().toStdString();
-  jsonFile["info"]["layout"]["version"] = layoutRoot.firstChild().nextSiblingElement("LayoutVersion").text().toStdString();
-  jsonFile["info"]["layout"]["developer"]["name"] = layoutRoot.firstChild().nextSiblingElement("DeveloperName").text().toStdString();
-  jsonFile["info"]["layout"]["developer"]["comment"] = UnescapeXML(layoutRoot.firstChild().nextSiblingElement("DeveloperComment").text()).toStdString();
-  // Images
-  QString image0 = layoutRoot.firstChild().nextSiblingElement("ImageNormalShift").text();
-  QString image1 = layoutRoot.firstChild().nextSiblingElement("ImageAltGrShift").text();
-  /* This removes many whitespaces and carriage returns('\r\n') thats are not needed */
-  jsonFile["info"]["layout"]["image0"] = image0.simplified().replace( " ", "" ).toStdString();
-  jsonFile["info"]["layout"]["image1"] = image1.simplified().replace( " ", "" ).toStdString();
-}
-
-void LayoutConverter::convertAvroKeydata() {
-  QDomElement keydata;
-  keydata = layoutRoot.firstChild().nextSiblingElement("KeyData");
-  for(QDomNode n = keydata.firstChild(); !n.isNull(); n = n.nextSibling())
+  while (!xmlReader->atEnd() && !xmlReader->hasError())
   {
-    QString key = n.nodeName();
-    /* Naming conversions */
-    if(key.contains("OEM1")) key.replace("OEM1", "Semicolon");
-    if(key.contains("OEM2")) key.replace("OEM2", "Slash");
-    if(key.contains("OEM3")) key.replace("OEM3", "BackQuote");
-    if(key.contains("OEM4")) key.replace("OEM4", "OpenBracket");
-    if(key.contains("OEM5")) key.replace("OEM5", "BackBracket");
-    if(key.contains("OEM6")) key.replace("OEM6", "CloseBracket");
-    if(key.contains("OEM7")) key.replace("OEM7", "Quote");
-    if(key.contains("MINUS")) key.replace("MINUS", "Minus");
-    if(key.contains("PLUS")) key.replace("PLUS", "Equals");
-    if(key.contains("PERIOD")) key.replace("PERIOD", "Period");
-    if(key.contains("COMMA")) key.replace("COMMA", "Comma");
+    xmlReader->readNext();
+    if (xmlReader->isStartDocument())
+    {
+      continue;
+    }
 
-    jsonFile["layout"][key.toStdString()] = UnescapeXML(n.toElement().text()).toStdString();
+    if (xmlReader->isStartElement())
+    {
+      QStringRef name = xmlReader->name();
+      if (name == "Layout" || name == "KeyData" || name == "AvroKeyboardVersion")
+      {
+        continue;
+      }
+
+      QString data = xmlReader->readElementText().trimmed();
+      if (!name.contains("Key") && !name.contains("Num"))
+      {
+        // Meta data conversion
+        if (name == "LayoutName")
+        {
+          infoLayout["name"] = data;
+        }
+        else if (name == "LayoutVersion")
+        {
+          infoLayout["version"] = data;
+        }
+        else if (name == "DeveloperName")
+        {
+          layoutDev["name"] = data;
+        }
+        else if (name == "DeveloperComment")
+        {
+          layoutDev["comment"] = unescapeXML(data);
+        }
+        else if (name == "ImageNormalShift")
+        {
+          infoLayout["image0"] = data.simplified().replace(" ", "");
+        }
+        else if (name == "ImageAltGrShift")
+        {
+          infoLayout["image1"] = data.simplified().replace(" ", "");
+        }
+      }
+      else
+      {
+        // Key conversion
+        QString key = name.toString();
+        if (key.contains("OEM1"))
+          key.replace("OEM1", "Semicolon");
+        if (key.contains("OEM2"))
+          key.replace("OEM2", "Slash");
+        if (key.contains("OEM3"))
+          key.replace("OEM3", "BackQuote");
+        if (key.contains("OEM4"))
+          key.replace("OEM4", "OpenBracket");
+        if (key.contains("OEM5"))
+          key.replace("OEM5", "BackBracket");
+        if (key.contains("OEM6"))
+          key.replace("OEM6", "CloseBracket");
+        if (key.contains("OEM7"))
+          key.replace("OEM7", "Quote");
+        if (key.contains("MINUS"))
+          key.replace("MINUS", "Minus");
+        if (key.contains("PLUS"))
+          key.replace("PLUS", "Equals");
+        if (key.contains("PERIOD"))
+          key.replace("PERIOD", "Period");
+        if (key.contains("COMMA"))
+          key.replace("COMMA", "Comma");
+
+        keys[key] = unescapeXML(data);
+      }
+    }
   }
+
+  info["type"] = "fixed";
+  info["version"] = "1";
+  infoLayout["developer"] = layoutDev;
+  info["layout"] = infoLayout;
+
+  QJsonObject layout;
+  layout["info"] = info;
+  layout["layout"] = keys;
+
+  QFileInfo fileInfo(xmlFile);
+  QString savePath = folders.getUserLayoutPath() + fileInfo.baseName() + ".json";
+  saveLayout(layout, savePath);
+
+  xmlReader->clear();
+  xmlFile.close();
+  delete xmlReader;
+}
+
+void LayoutConverter::saveLayout(QJsonObject obj, QString path) 
+{
+  QFile saveFile(path);
+    saveFile.open(QIODevice::WriteOnly);
+
+    QJsonDocument json(obj);
+    saveFile.write(json.toJson());
+    saveFile.close();
 }
--- src/frontend/LayoutConverter.h
+++ src/frontend/LayoutConverter.h
@@ -1,6 +1,6 @@
 /*
  *  OpenBangla Keyboard
- *  Copyright (C) 2016 Muhammad Mominul Huque <mominul2082@gmail.com>
+ *  Copyright (C) 2016-2018 Muhammad Mominul Huque <mominul2082@gmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,25 +16,18 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <fstream>
 #include <QFile>
-#include <QtXml>
-#include "json.hpp"
+#include <QJsonObject>
 #include "SpecialFolders.h"
 
 class LayoutConverter {
-  QFile conversionFile;
-  QByteArray data;
-  std::ofstream convertedFile;
-  QDomElement layoutRoot;
-  nlohmann::json jsonFile;
   SpecialFolders folders;
 
   void convertAvroLayout(QString path);
-  void convertAvroInfo();
-  void convertAvroKeydata();
+  
+  void saveLayout(QJsonObject obj, QString path);
 
-  QString UnescapeXML(QString escaped);
+  QString unescapeXML(QString escaped);
 public:
   void convertLayout(QString path);
 };
