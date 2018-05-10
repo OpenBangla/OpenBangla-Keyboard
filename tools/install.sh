6d1a888479cad18b8e0434691f41be979a73fd10
Adyel Ullahil Mamun <adyel@live.com>
2018-05-10 21:45:15 +0600

    sudo before rm -f

--- tools/install.sh
+++ tools/install.sh
@@ -12,19 +12,19 @@ case $os in
 ("Ubuntu")
       wget -q --show-progress https://github.com/OpenBangla/OpenBangla-Keyboard/releases/download/1.4.0/OpenBangla-Keyboard_1.4.0-ubuntu18.04.deb -O ./OpenBangla.deb
       sudo apt install ./OpenBangla.deb
-      rm -f ./OpenBangla.deb
+      sudo rm -f ./OpenBangla.deb
       ;;
 
 ("arch")
       wget -q --show-progress https://github.com/OpenBangla/OpenBangla-Keyboard/releases/download/1.4.0/openbangla-keyboard-1.4.0-1-x86_64-archlinux.pkg.tar.xz -O /tmp/OpenBangla.pkg.tar.xz
       sudo pacman -U /tmp/OpenBangla.pkg.tar.xz
-      rm -f /tmp/OpenBangla.pkg.tar.xz
+      sudo rm -f /tmp/OpenBangla.pkg.tar.xz
       ;;
       
 ("Fedora")
       wget -q --show-progress https://github.com/OpenBangla/OpenBangla-Keyboard/releases/download/1.4.0/OpenBangla-Keyboard_1.4.0-fedora27.rpm -O /tmp/OpenBangla.rpm
       sudo dnf install /tmp/OpenBangla.rpm
-      rm -f /tmp/OpenBangla.rpm
+      sudo rm -f /tmp/OpenBangla.rpm
       ;;
 
 (*)
