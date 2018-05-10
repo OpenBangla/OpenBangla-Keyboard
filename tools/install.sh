0404ee43d86d4438253d0ff8347120a6a9982b55
Adyel Ullahil Mamun <adyel@live.com>
2018-05-10 21:46:49 +0600

    remove rm for arch

--- tools/install.sh
+++ tools/install.sh
@@ -18,7 +18,6 @@ case $os in
 ("arch")
       wget -q --show-progress https://github.com/OpenBangla/OpenBangla-Keyboard/releases/download/1.4.0/openbangla-keyboard-1.4.0-1-x86_64-archlinux.pkg.tar.xz -O /tmp/OpenBangla.pkg.tar.xz
       sudo pacman -U /tmp/OpenBangla.pkg.tar.xz
-      sudo rm -f /tmp/OpenBangla.pkg.tar.xz
       ;;
       
 ("Fedora")
