24d326f308729005810092ecca4f540a7b50b777
Adyel Ullahil Mamun <adyel@live.com>
2018-05-10 21:31:16 +0600

    Add Fedora

--- tools/install.sh
+++ tools/install.sh
@@ -2,7 +2,7 @@
 
 # detect OS
 os=$(python3 -c "exec(\"import platform\\nprint(platform.linux_distribution()[0])\")" )
-echo $os
+echo Downloading $os Binary
 
 # get super user access 
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
@@ -20,6 +20,12 @@ case $os in
       wget -q --show-progress https://github.com/OpenBangla/OpenBangla-Keyboard/releases/download/1.4.0/openbangla-keyboard-1.4.0-1-x86_64-archlinux.pkg.tar.xz -O /tmp/OpenBangla.pkg.tar.xz
       pacman -U /tmp/OpenBangla.pkg.tar.xz
       rm -f /tmp/OpenBangla.pkg.tar.xz
+      ;;
+      
+("Fedora")
+      wget -q --show-progress https://github.com/OpenBangla/OpenBangla-Keyboard/releases/download/1.4.0/OpenBangla-Keyboard_1.4.0-fedora27.rpm -O /tmp/OpenBangla.rpm
+      sudo dnf install /tmp/OpenBangla.rpm
+      rm -f /tmp/OpenBangla.rpm
       ;;
 
 (*)
