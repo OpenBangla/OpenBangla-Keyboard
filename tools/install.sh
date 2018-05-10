0abdf79a1e4cae005cbff07033e0f3fba6ec9d4a
Adyel Ullahil Mamun <adyel@live.com>
2018-05-10 17:06:44 +0600

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -8,7 +8,7 @@ echo $os
 case $os in
 
 ("Ubuntu")
-      wget https://github.com/OpenBangla/OpenBangla-Keyboard/releases/download/1.4.0/OpenBangla-Keyboard_1.4.0-ubuntu18.04.deb -O OpenBangla.deb
+      wget https://github.com/OpenBangla/OpenBangla-Keyboard/releases/download/1.4.0/OpenBangla-Keyboard_1.4.0-ubuntu18.04.deb -O ./OpenBangla.deb
       sudo apt install ./OpenBangla.deb
       rm -f ./OpenBangla.deb
       ;;
