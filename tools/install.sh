62579917466032121990f0417bae53016ea5ce07
Muhammad Mominul Huque <mominul2082@gmail.com>
2018-06-25 22:59:50 +0600

    💄 install.sh 💄 (#43)
    * Update install.sh
    
    * Download package according to Ubuntu version.
    
    * remove python dependency from install script (#53)

--- tools/install.sh
+++ tools/install.sh
@@ -1,39 +1,87 @@
 #!/bin/bash
+## MUST: Always update this whenever a new version is released. Also remember to update download URLs if needed
+APP_VERSION=1.4.0
+URL_STUB="https://github.com/OpenBangla/OpenBangla-Keyboard/releases/download/${APP_VERSION}"
 
-# set Download link
+cat <<"EOF"
+|-------------------------------------------------------|
+|-----Welcome to the OpenBangla Keyboard installer------|
+|-------------------------------------------------------|
 
-ubuntuLink="https://github.com/OpenBangla/OpenBangla-Keyboard/releases/download/1.4.0/OpenBangla-Keyboard_1.4.0-ubuntu18.04.deb"
-archLink="https://github.com/OpenBangla/OpenBangla-Keyboard/releases/download/1.4.0/openbangla-keyboard-1.4.0-1-x86_64-archlinux.pkg.tar.xz"
-fedoraLink="https://github.com/OpenBangla/OpenBangla-Keyboard/releases/download/1.4.0/OpenBangla-Keyboard_1.4.0-fedora27.rpm"
+This helper script will install/update OpenBangla Keyboard in your system.
+You might need to type your password for the installation to proceed.
 
-# detect OS
-os=$(python -c "exec(\"import platform\\nprint(platform.linux_distribution()[0])\")" )
-echo Downloading $os Binary
+After the installation completes successfully, you would need to configure
+your desktop environment for OpenBangla Keyboard to work.
+Visit https://github.com/OpenBangla/OpenBangla-Keyboard/wiki/Configuring-Environment
+for instructions on how to configure your desktop environment.
+--------------------------------------------------------
 
-# get super user access 
+EOF
 
+# detect distro
+function get_distro {
+  local distro_name distro_data distro_str
+  # try to get distro data from *-release files
+  distro_data=$(cat /etc/*-release)
+  while read -r line; do
+      if [[ $line =~ ^(NAME|DISTRIB_ID)=(.+)$ ]]; then
+      distro_str=${BASH_REMATCH[2]}
+      if echo "$distro_str" | grep -qP '(?i).*ubuntu.*'; then
+        distro_name='ubuntu'
+      elif echo "$distro_str" | grep -qP '(?i).*fedora.*'; then
+        distro_name='fedora'
+      elif echo "$distro_str" | grep -qP '(?i)^[''"]?(arch|manjaro|antergos|chakra|magpie|parabola|anarchy).*'; then
+        distro_name='arch'
+      fi
+      [ $distro_name ] && break
+    fi
+  done <<< "$distro_data"
+  # plain arch installation doesn't guarantee /etc/os-release
+  # but the filesystem pkg installs a blank /etc/arch-release
+  if [ -z "$distro_name" ]; then
+    if [ -e /etc/arch-release ]; then
+      distro_name='arch'
+    fi
+  fi
+  # TODO if os not detected, try package-manager detection (not 100% reliable)
+  echo "$distro_name"
+}
+
+DISTRO_NAME=$(get_distro)
+echo "Downloading installation package for $DISTRO_NAME ..."
 # start downloading and install
-case $os in
-
-("Ubuntu")
-      wget -q --show-progress $ubuntuLink -O ./OpenBangla.deb
-      sudo apt install ./OpenBangla.deb
-      sudo rm -f ./OpenBangla.deb
-      ;;
-
+case $DISTRO_NAME in
+("ubuntu")
+  # VERSION_ID=$(lsb_release -r | cut -f 2)  #  lsb_release may be replaced as well with shell script
+  # ubuntu is supposed to ensure a /etc/os-release file by default
+  # VERSION_ID (and some more vars) are sourced
+  # shellcheck disable=SC1091
+  source /etc/os-release
+  if [[ $VERSION_ID = "18.04" || $VERSION_ID = "16.04" ]]; then
+    wget -q --show-progress "${URL_STUB}/OpenBangla-Keyboard_${APP_VERSION}-ubuntu${VERSION_ID}.deb" -O "$HOME/OpenBangla.deb"
+    sudo apt install "$HOME/OpenBangla.deb"
+    mv -f "$HOME/OpenBangla.deb" /tmp/
+  else
+    echo "Ubuntu version not supported ($VERSION_ID)."
+  fi
+  ;;
 ("arch")
-      wget -q --show-progress $archLink -O /tmp/OpenBangla.pkg.tar.xz
-      sudo pacman -U /tmp/OpenBangla.pkg.tar.xz
-      ;;
-      
-("Fedora")
-      wget -q --show-progress $fedoraLink -O /tmp/OpenBangla.rpm
-      sudo dnf install /tmp/OpenBangla.rpm
-      sudo rm -f /tmp/OpenBangla.rpm
-      ;;
-
+  # this url should proably be normalized soon
+  wget -q --show-progress "${URL_STUB}/openbangla-keyboard-${APP_VERSION}-1-x86_64-archlinux.pkg.tar.xz" -O /tmp/OpenBangla.pkg.tar.xz
+  sudo pacman -U /tmp/OpenBangla.pkg.tar.xz
+  ;;
+("fedora")
+  # fedora version check ?
+  wget -q --show-progress "${URL_STUB}/OpenBangla-Keyboard_${APP_VERSION}-fedora27.rpm" -O /tmp/OpenBangla.rpm
+  sudo dnf install /tmp/OpenBangla.rpm
+  ;;
 (*)
-      echo "Could Not Find your distro"
-      ;;
-
+  echo
+  echo "Sorry, this script was not able to identify your OS distribution."
+  echo
+  echo "Please consult https://github.com/OpenBangla/OpenBangla-Keyboard/wiki/"
+  echo "for distrowise install or build instructions."
+  echo
+  ;;
 esac
