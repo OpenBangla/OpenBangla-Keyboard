536fb5b15fc9ae0b94aa2d8aaf8d5859fdf2ea9f
Muhammad Mominul Huque <mominul2082@gmail.com>
2019-06-22 01:46:30 +0600

    Fix the panic and make the engine a C++ project again! update submodule

--- src/engine/ibus-new/main.c
+++
@@ -1,9 +0,0 @@
-#include "stdio.h"
-#include "libriti.h"
-
-int main(int argc, char* argv[]) {
-    printf("\nWorking!!\n");
-    RitiContext *ctx = riti_context_new();
-    riti_context_free(ctx);
-	return 0;
-}
---
+++ src/engine/ibus-new/main.cpp
@@ -0,0 +1,16 @@
+#include <QDebug>
+#include "riti.h"
+#include "Settings.h"
+
+void update_with_settings() {
+    qputenv("RITI_LAYOUT_FILE", gSettings->getLayoutPath().toLatin1());
+}
+
+int main(int argc, char* argv[]) {
+    gSettings = new Settings();
+    update_with_settings();
+    RitiContext *ctx = riti_context_new();
+    qDebug() << riti_context_key_handled(ctx);
+    riti_context_free(ctx);
+	return 0;
+}
