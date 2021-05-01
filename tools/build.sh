b6ff14b906e77d4d1ea08b73e3f7220c5c5d8a3b
Muhammad Mominul Huque <mominul2082@gmail.com>
2021-05-01 20:13:29 +0600

    `install.sh` again :pensive: (#211)
    * `install.sh` again :pensive:
    
    * Update README
    
    pkg+

--- tools/build.sh
+++ tools/build.sh
@@ -44,8 +44,8 @@ if [[ $DIST =~ ^(ubuntu|debian) ]]; then
     apt-get -qq update
     apt-get -y install git
     # this is to read distro codename from filename during deployment
-    CODENAME=$(cat /etc/os-release | grep "VERSION_CODENAME" | cut -d= -f2)
-    DIST="${DIST}-${CODENAME}"
+    #CODENAME=$(cat /etc/os-release | grep "VERSION_CODENAME" | cut -d= -f2)
+    #DIST="${DIST}-${CODENAME}"
     BUILDFUNC=makeDeb
 elif [[ $DIST =~ ^fedora ]]; then
     dnf -y --allowerasing distro-sync
