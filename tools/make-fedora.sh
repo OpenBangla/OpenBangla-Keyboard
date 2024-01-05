35fb471261e618171273e48fcddb56d190fa129e
larina3315 <larina3315@tutanota.com>
2024-01-06 01:14:21 +0600

    Refactor and Revise the script
    Refactor and Revise the script

--- tools/make-fedora.sh
+++ tools/make-fedora.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Generating Fedora packages..."
-source ~/.obk-build/config.bash
+source ~/.obk-build/config.bash > /dev/null 2>&1
 cd ~/.obk-build/
 
 #update the system and install builddeps
@@ -8,27 +8,26 @@ sudo dnf -y update
 sudo dnf -y install @buildsys-build rust cargo cmake qt5-qtdeclarative-devel libzstd-devel fcitx5 fcitx5-devel ibus ibus-devel ninja-build curl meson
 
 #git clone obk, update if exists
-if [ -d "~/.obk-build/OpenBangla-Keyboard/" ]; then
-  cd OpenBangla-Keyboard
+if [ -d ~/.obk-build/OpenBangla-Keyboard ]; then
+  cd OpenBangla-Keyboard && git pull
 else
   git clone --recursive https://github.com/OpenBangla/OpenBangla-Keyboard.git && cd OpenBangla-Keyboard
 fi
 #determine branch
-if [ $BRANCH_OBK = 'develop' ]; then
+if [ "$BRANCH_OBK" = 'develop' ]; then
   git checkout develop
 else
   git checkout master
 fi
-#update submodules and repo
-git pull --force
+#update submodules
 git submodule update
 
 #compile for ibus
-if [ $IM_IBUS_OBK = 'YES' ]; then
+if [ "$IM_IBUS_OBK" = 'YES' ]; then
   mkdir build-ibus-fedora
   cmake -Bbuild-ibus-fedora -GNinja -DCPACK_GENERATOR=RPM -DCMAKE_INSTALL_PREFIX="/usr" -DENABLE_IBUS=ON && ninja package -C build-ibus-fedora
 fi
-if [ $IM_FCITX_OBK = 'YES' ]; then
+if [ "$IM_FCITX_OBK" = 'YES' ]; then
   mkdir build-fcitx-fedora
   cmake -Bbuild-fcitx-fedora -GNinja -DCPACK_GENERATOR=RPM -DCMAKE_INSTALL_PREFIX="/usr" -DENABLE_FCITX=ON && ninja package -C build-fcitx-fedora
 fi
--- tools/make-pkgs.sh
+++ tools/make-pkgs.sh
@@ -1,61 +1,93 @@
 #!/bin/bash
-rm -r ~/.obk-build/config.bash
-mkdir ~/.obk-build/
-touch ~/.obk-build/config.bash
+rm -r ~/.obk-build/config.bash > /dev/null 2>&1
+mkdir ~/.obk-build/ > /dev/null 2>&1
+touch ~/.obk-build/config.bash > /dev/null 2>&1
 
+#cleanup function
+#help function
+#storing args
+DISTRO_COUNT=0
 for arg in "$@"; do
-    case $arg in
-        --ibus)
-          echo 'IM_IBUS_OBK='YES'' >> ~/.obk-build/config.bash
-            ;;
-        --fcitx)
-          echo 'IM_FCITX_OBK='YES'' >> ~/.obk-build/config.bash 
-            ;;
-        --develop)
-          echo 'BRANCH_OBK='develop'' >> ~/.obk-build/config.bash
-            ;;
-        --toolbox)
-          echo 'TOOLBOX_ENABLE_OBK='YES'' >> ~/.obk-build/config.bash
-            ;;
-        --fedora)
-          echo 'FEDORA_OBK='YES'' >> ~/.obk-build/config.bash
-            ;;
-        --debian)
-          echo 'DEBIAN_OBK='YES'' >> ~/.obk-build/config.bash
-            ;;
-        *)
-            echo "Invalid argument: $arg"
-            exit 1
-            ;;
+  case $arg in
+    ("--ibus") echo 'IM_IBUS_OBK='YES'' >> ~/.obk-build/config.bash ;;
+    ("--fcitx") echo 'IM_FCITX_OBK='YES'' >> ~/.obk-build/config.bash ;;
+    ("--develop") echo 'BRANCH_OBK='develop'' >> ~/.obk-build/config.bash ;;
+    ("--toolbox") echo 'TOOLBOX_ENABLE_OBK='YES'' >> ~/.obk-build/config.bash ;;
+    ("--fedora")
+      echo 'FEDORA_OBK='YES'' >> ~/.obk-build/config.bash
+      echo "FEDORA_VERSION_OBK="$(echo "$@" | sed -n 's/.*--fedora \([0-9]*\).*/\1/p')"" >> ~/.obk-build/config.bash
+      ((DISTRO_COUNT++))
+        ;;
+    ("--debian")
+      echo 'DEBIAN_OBK='YES'' >> ~/.obk-build/config.bash
+      echo "DEBIAN_VERSION_OBK="$(echo "$@" | sed -n 's/.*--debian \([0-9]*\).*/\1/p')"" >> ~/.obk-build/config.bash
+      ((DISTRO_COUNT++))
+         ;;
+    (--*)
+      echo "Unknown argument: $arg"
+      echo "Exiting..."
+      exit 1
+         ;;
     esac
 done
 
 #load vars from file
-source ~/.obk-build/config.bash
+source ~/.obk-build/config.bash 2>&1
+#cat ~/.obk-build/config.bash
 
 #if user wants to use a toolbox
-if [ $TOOLBOX_ENABLE_OBK = 'YES' ]; then
+if [ "$TOOLBOX_ENABLE_OBK" = 'YES' ]; then
   #create toolbox
   echo "Creating toolbox..."
-  if [ $FEDORA_OBK = 'YES' ]; then
-    toolbox create obk-toolbox-fedora -d fedora
+
+  #fedora toolbox
+  if [ "$FEDORA_OBK" = 'YES' ]; then
     chmod +x make-fedora.sh
-    toolbox run -c obk-toolbox-fedora ./make-fedora.sh
+    if [ -z "$FEDORA_VERSION_OBK" ]; then
+    #if the user didnt specified a version
+        toolbox create obk-toolbox-fedora -d fedora
+        toolbox run -c obk-toolbox-fedora ./make-fedora.sh
+        exit 1
+    else
+    #if user did
+        toolbox create obk-toolbox-fedora-"$FEDORA_VERSION_OBK" -d fedora -r "$FEDORA_VERSION_OBK"
+        toolbox run -c obk-toolbox-fedora-"$FEDORA_VERSION_OBK" ./make-fedora.sh
+        exit 1
+    fi
   fi
-  if [ $DEBIAN_OBK = 'YES' ]; then
-    toolbox create obk-toolbox-debian -i quay.io/toolbx-images/debian-toolbox
+
+  #debian toolbox
+  if [ "$DEBIAN_OBK" = 'YES' ]; then
     chmod +x make-debian.sh
-    toolbox run -c obk-toolbox-debian ./make-debian.sh
+    if [ -z "$DEBIAN_VERSION_OBK" ]; then
+    #if the user didnt specified a version
+        toolbox create obk-toolbox-debian -i quay.io/toolbx-images/debian-toolbox
+        toolbox run -c obk-toolbox-debian ./make-debian.sh
+        exit 1
+    else
+    #if user did
+        toolbox create obk-toolbox-debian-"$DEBIAN_VERSION_OBK" -i quay.io/toolbx-images/debian-toolbox:"$DEBIAN_VERSION_OBK"
+        toolbox run -c obk-toolbox-debian-"$DEBIAN_VERSION_OBK" ./make-debian.sh
+        exit 1
+    fi
   fi
+fi
+
 #if user doesn't want ot use a toolbox
+elif [[ "$DISTRO_COUNT" -ne 1 ]]; then
+      echo "Cannot use more than one distro without --toolbox flag."
+      echo "Exiting..."
+      exit 1
 else
-  if [ $FEDORA_OBK = 'YES' ]; then
+  if [ "$FEDORA_OBK" = 'YES' ]; then
     chmod +x make-fedora.sh
     ./make-fedora.sh
+    exit 1
   fi
-  if [ $DEBIAN_OBK = 'YES' ]; then
+  if [ "$DEBIAN_OBK" = 'YES' ]; then
     chmod +x make-debian.sh
     ./make-debian.sh
+    exit 1
   fi
 fi
 
